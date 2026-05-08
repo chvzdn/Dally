8be9ae957609fb30212032d569b381a1cbf50ce4
Renelyn Dino <maycdino24@gmail.com>
2026-05-08 18:53:29 +0800

    Update schedule_service_impl.py

--- schedule_service_impl.py
+++ schedule_service_impl.py
@@ -1,6 +1,11 @@
 from interfaces.data_service import IDataService
+from models.schedule import Schedule
+
 
 class ScheduleService(IDataService):
+    """
+    Handles scheduling operations (date + time + task)
+    """
 
     def __init__(self):
         self._schedule = []
@@ -9,17 +14,19 @@ class ScheduleService(IDataService):
         self._schedule.append(item)
 
     def assign(self, task, time, date):
-        self._schedule.append({
-            "task": task,
-            "time": time,
-            "date": date
-        })
+        # Adds a schedule entry
+        if task.strip() and time.strip() and date.strip():
+            self._schedule.append(
+                Schedule(task, time, date)
+            )
 
     def delete(self, index):
+        # Deletes schedule entry
         if 0 <= index < len(self._schedule):
             self._schedule.pop(index)
 
     def clear(self):
+        # Removes all schedules
         self._schedule.clear()
 
     def get_all(self):
