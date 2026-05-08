30d9304602490d68830f5660b29ce581d37d2623
Renelyn Dino <maycdino24@gmail.com>
2026-05-08 18:52:25 +0800

    Update notes_service_impl.py

--- notes_service_impl.py
+++ notes_service_impl.py
@@ -1,14 +1,24 @@
 from interfaces.data_service import IDataService
+from models.note import Note
+
 
 class NotesService(IDataService):
+    """
+    Handles note operations
+    Implements IDataService (polymorphism)
+    """
 
     def __init__(self):
+        # Internal storage for notes
         self._notes = []
 
     def add(self, item):
-        self._notes.append(item)
+        # Adds a new note if input is not empty
+        if item.strip():
+            self._notes.append(Note(item))
 
     def delete(self, index):
+        # Removes note if index is valid
         if 0 <= index < len(self._notes):
             self._notes.pop(index)
 
