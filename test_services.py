1634192a4a6b00905512a54e4bbac8125b09c5a2
Renelyn Dino <maycdino24@gmail.com>
2026-05-08 19:08:47 +0800

    Update test_services.py

--- test_services.py
+++ test_services.py
@@ -1,6 +1,36 @@
 from services.task_service_impl import TaskService
+from services.schedule_service_impl import ScheduleService
+from services.notes_service_impl import NotesService
 
-def test_add():
-    t = TaskService()
-    t.add("Test")
-    assert len(t.get_all()) == 1
+# TEST: Add task
+def test_add_task():
+    service = TaskService()
+    service.add("Study for Math Exam")
+    assert len(service.get_all()) == 1
+
+# TEST: Toggle task completion
+def test_toggle_task():
+    service = TaskService()
+    service.add("Study for Math Exam")
+    service.toggle(0, True)
+    assert service.get_all()[0].completed is True
+
+# TEST: Delete task
+def test_delete_task():
+    service = TaskService()
+    service.add("Study for Math Exam")
+    service.delete(0)
+    assert len(service.get_all()) == 0
+
+# TEST: Add note
+def test_add_note():
+    service = NotesService()
+    service.add("Reminder:Wash Dishes")
+    assert len(service.get_all()) == 1
+
+# TEST: Clear schedule
+def test_clear_schedule():
+    service = ScheduleService()
+    service.assign("Math", "3PM", "Exam")
+    service.clear()
+    assert len(service.get_all()) == 0
